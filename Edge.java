a5da7ea304c9965e22aa10e53b91eba7bbb004d2
Ginestra <alex.ginestra4@etu.univ-lorraine.fr>
2018-01-29 22:54:37 +0100

    creation des fichiers pour le src

---
+++ Edge.java
@@ -0,0 +1,15 @@
+package reducteur;
+
+public class Edge
+{
+   int from;
+   int to;
+   int cost;
+   Edge(int x, int y, int cost)
+	 {
+		this.from = x;
+		this.to = y;
+		this.cost = cost;
+	 }
+   
+}
