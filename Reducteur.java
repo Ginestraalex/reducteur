06a5c5df2538c8d7d36780a0b136c7f78ad7e143
Ginestra <alex.ginestra4@etu.univ-lorraine.fr>
2018-02-02 15:22:27 +0100

    finalisation parite 1

--- Reducteur.java
+++ Reducteur.java
@@ -1,6 +1,9 @@
-package reducteur;
+import java.awt.Component;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-import java.util.ArrayList;
+import reducteur.SeamCarving;
 
 public class Reducteur {
 	
@@ -9,15 +12,33 @@ public class Reducteur {
 	}
 	
 	public static void main(String... args) {
-		if(args.length != 1) {
-			System.out.println("Vous devez donnez le nom d'un fichier en paramètre");
+		SeamCarving sc = new SeamCarving();
+		if(args.length == 0) {
+		    Component parent = null;
+			JFileChooser chooser = new JFileChooser();
+		    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+		        "PGM Images", "pgm");
+		    chooser.setFileFilter(filter);
+			int returnVal = chooser.showOpenDialog(parent);
+		    if(returnVal == JFileChooser.APPROVE_OPTION) {
+		    		String[] selection = {"1","2","3","4","5","6","7","8","9"};
+			    	String retour = (String) JOptionPane.showInputDialog(null, "Choisissez un indice", "Boite de dialogue",JOptionPane.QUESTION_MESSAGE, null, selection, selection[0]);
+			    	if(retour != null) {
+			    		sc.reducePict(chooser.getSelectedFile().getPath(), true, Integer.parseInt(retour));
+					JOptionPane.showMessageDialog(null, "Transformation de l'image terminé");
+			    	}
+		    }
 		}
-		else{
-			SeamCarving sc = new SeamCarving();
-			int[][] tabImage = sc.readpgm(args[0]);
-			int[][] itr = sc.interest(tabImage);
-			Graph gr = sc.tograph(itr);
-			ArrayList<Integer>parcourt = sc.Dijkstra(gr, 0, gr.vertices()-1);
+		else if(args.length == 1){
+			sc.reducePict(args[0], false, 1);
+			JOptionPane.showMessageDialog(null, "Transformation de l'image terminé");
+		}
+		else if(args.length == 2) {
+			sc.reducePict(args[0], false, Integer.parseInt(args[1]));
+			JOptionPane.showMessageDialog(null, "Transformation de l'image terminé");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "vous devez faire:\n java -jar nomProgramme.jar nomfichier nbDePixelASupp");
 		}
 	}
 }
